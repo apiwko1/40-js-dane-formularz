ff1ee5da950fc2f25edf92616eeb0acd9571cd56
apiwko1 <70264036+apiwko1@users.noreply.github.com>
2022-07-19 09:14:16 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 function uzupelnij(){
     let fname = document.getElementById('fname');
-    let name = document.querySelector('#name');
+    let name = document.getElementById('name');    
+
+    let fsurname = document.getElementById('fsurname');
+    let surname = document.getElementById('surname');
+
+    let fphone = document.getElementById('fphone');
+    let phone = document.getElementById('phone');
+
     name.textContent = fname.value;
+    surname.textContent = fsurname.value;
+    phone.textContent = fphone.value;
 
 }
 
